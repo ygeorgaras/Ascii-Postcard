887834a9708f392d13c7b6c1ee151c0f23d1f8f1
ygeorgaras <ygeorgaras@gmail.com>
2024-04-12 09:54:40 -0400

    Creating basic ascii image, looking into new ways to smooth edges with
    + Create ascii darkness scale
    + Use darkness vals to generate ascii string

--- Ascii_Postcard/AsciiPostcard.py
+++ Ascii_Postcard/AsciiPostcard.py
@@ -2,35 +2,54 @@ from PIL import Image
 
 # Generates and returns greyscale image
 def genGreyscaleImage():
-    tempImg = Image.open("thailand.png").convert('L')
+    tempImg = Image.open("C:/Python_Projects/Ascii-Postcard/Ascii_Postcard/sunset.jpg").convert('L')
     return tempImg
 
 # Calculate the darkness of a certain range of pixels
-def getDarkness(x_start, y_start):
-    brightness_total = 0
+def getDarkness(xStart, yStart):
+    brightnessTotal = 0
     # Check each pixel and add it's value to brightness_total
-    for x in range(x_start, x_start + char_width):
-        for y in range(y_start, y_start + char_height):
-            brightness = img.getpixel((x, y))
-            brightness_total += brightness
-    avg_brightness = brightness_total // num_pixels_cell
+    for x in range(xStart, xStart + charWidth):
+        if(x < img.width):
+            for y in range(yStart, yStart + charHeight):
+                if(y < img.height):
+                    brightness = img.getpixel((x, y))
+                    brightnessTotal += brightness
+    avgBrightness = brightnessTotal // numPixelsCell
 
     # Return the avg_brightness of the cell
-    return 255 - avg_brightness
+    return 255 - avgBrightness
+
+# Create and return darkness 
+def getAsciiValuesMap():
+    asciiMap = {}
+    rowPanels = (img.height // charHeight)  # Number of characters that fit vertically
+    colPanels = (img.width // charWidth)   # Number of characters that fit horizontally
+    for y in range(rowPanels):
+        for x in range(colPanels):
+            # Add each row to the ascii darkness map
+            asciiMap[(y,x)] = getDarkness(x * charWidth, y * charHeight)
+    return asciiMap
+
+# Function to map a brightness value (0-255) to an ASCII character
+def brightnessToAscii(brightness):
+    index = int(brightness / 255 * (len(ascii_chars) - 1))
+    return ascii_chars[index]
 
 img = genGreyscaleImage()
-img.save("greyscale.png")
-pixel_row, pixel_col = img.size         # Get the size of the input image
-char_width = 12
-char_height = 12
-num_pixels_cell = char_width * char_height
-rowPanels = (pixel_row // char_height)  # Number of characters that fit vertically
-colPanels = (pixel_col // char_width)   # Number of characters that fit horizontally
-ascii_values_map = {}                   # Store a tuple(row,col) with the darkness value
+#img.save("greyscale.png")
+pixelRow, pixelCol = img.size         # Get the size of the input image
+charWidth = 6
+charHeight = 12
+numPixelsCell = charWidth * charHeight
+ascii_values_map = getAsciiValuesMap()# Store a tuple(row,col) with the darkness value
+ascii_chars = [' ', '.', '`', '\'', ':', '-', '!', '~', '*', '=', '+', '<', '>', 'i', 'l', 't', 'f', 'x', 'n', 'o', 'a', 'e', 'r', 'u', 'v', 'w', 'z', 'X', 'Y', 'U', 'J', 'C', 'L', 'Q', '0', 'Z', 'm', 'q', 'p', 'd', 'b', 'k', 'h', 'A', 'R', 'O', 'P', 'G', 'B', '8', '&', '%', '$', '#', '@']
+asciiString = ""
+prevRow = -1
 
-for i in range(rowPanels):
-    next_row_size = i * char_width
-    for j in range(colPanels):
-        next_col_size = j * char_height
-        # Add each row to the ascii darkness map
-        ascii_values_map[(i,j)] = getDarkness(next_row_size, next_col_size)
+for (y, x), brightness in sorted(ascii_values_map.items()):
+    if prevRow != y:
+        if prevRow != -1:  # Avoid adding a newline on the first line
+            asciiString += "\n"
+        prevRow = y
+    asciiString += brightnessToAscii(brightness)
--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 + We segment the image into panels
 + Calculate the darkness of a panel by counting each pixel
 + Store the darkness in a map((x,y) darkness) to use later
-- Create ascii darkness scale
-- Use darkness vals to generate ascii string
++ Create ascii darkness scale
++ Use darkness vals to generate ascii string
++ Generate ascii art image
+
+- Investigating OpenCV to smooth edges for better result
 - Add custom text option
 - Add location placement for text
